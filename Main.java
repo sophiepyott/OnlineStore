b434dc30d4fd00924b7a2803affb87e48357f939
menchukf <77518360+menchukf@users.noreply.github.com>
2024-09-30 16:08:25 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+   //Your tests go here! I expect you to make sure various parts of your program work. 
+
+     public static void main(String[] args)
+     {
+        Store s = new Store();
+        Book b = new Book();
+        System.out.println(b instanceof ItemForSale);
+     }
+}
